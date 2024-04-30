24c2b2f632045c755cae986513b34bd8c28d43a6
MuktadirHassan <tamimmuktadir@gmail.com>
2024-04-30 12:20:24 +0600

    parsing flags

--- main.go
+++ main.go
@@ -1,37 +1,132 @@
 package main
 
 import (
-	"bytes"
 	"flag"
-	"fmt"
-	"log"
-	"os/exec"
+	"time"
+
+	"os"
+
+	"github.com/charmbracelet/log"
+	"gopkg.in/yaml.v3"
 )
 
-var (
-	logger = log.Default()
-)
+var logger *log.Logger
 
 func main() {
-	logger.Println("Welcome to deployer!")
-	var nFlag = flag.Int("n", 1234, "Description of the flag")
+	// Setup logger
+	logger = log.NewWithOptions(os.Stderr, log.Options{
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.DateTime,
+		Prefix:          "🎵",
+		Level:           log.DebugLevel,
+	})
 
-	println(*nFlag)
+	logger.Debug("Welcome to deployer!")
+
+	// Parse flags
+	project := flag.String("project", "", "Project to deploy")
+	version := flag.String("version", "", "Version to deploy")
+	rollback := flag.Bool("rollback", false, "Rollback to previous version")
+
 	flag.Parse()
 
-	fmt.Println("word: ", *nFlag)
+	logger.Debug("Project: ", *project)
+	logger.Debug("Version: ", *version)
+	logger.Debug("Rollback: ", *rollback)
 
-	cmd := exec.Command("ls", "-lah")
-
-	var out bytes.Buffer
-
-	cmd.Stdout = &out
-	err := cmd.Run()
-
-	if err != nil {
-		logger.Fatal("ls error", err)
+	if *project == "" {
+		logger.Error("Project is required")
+		return
 	}
 
-	fmt.Println(out.String())
+	if *version == "" {
+		logger.Error("Version is required")
+		return
+	}
+
+	// Deploy
+	if !*rollback {
+		deploy(*project, *version)
+	} else {
+		rollbackVersion(*project, *version)
+	}
+}
+
+/**
+deployer --project=backend --version=1.2.3
+deployer --project=frontend --rollback=true --version=1.2.2
+
+Parse inputs -> get value of project, rollback, version etc
+if no rollback, call deploy func
+
+*/
+
+func deploy(project, version string) {
+	logger.Debug("Deploying project: ", project, " version: ", version)
+
+}
+
+func rollbackVersion(project, version string) {
+	logger.Debug("Rolling back project: ", project, " version: ", version)
+}
+
+type ComposeFile struct {
+	Version  string             `yaml:"version"`
+	Services map[string]Service `yaml:"services"`
+	Networks map[string]Network `yaml:"networks"`
+	Volumes  map[string]Volume  `yaml:"volumes"`
+}
+
+type Service struct {
+	Image       string   `yaml:"image"`
+	Ports       []string `yaml:"ports"`
+	Environment []string `yaml:"environment"`
+	Networks    []string `yaml:"networks"`
+	Volumes     []string `yaml:"volumes"`
+}
+
+type Network struct {
+	Driver string `yaml:"driver"`
+}
+
+type Volume struct {
+	Driver string `yaml:"driver"`
+}
+
+func generateComposeFile(project, version string) {
+	logger.Debug("Generating compose file for project: ", project, " version: ", version)
+
+	// Generate compose file with project and version
+	composeFile := ComposeFile{
+		Version: "3",
+		Services: map[string]Service{
+			project: {
+				Image:       project + ":" + version,
+				Ports:       []string{"8080:8080"},
+				Environment: []string{"ENV=PROD"},
+				Networks:    []string{"default"},
+				Volumes:     []string{"./" + project + "/data:/data"},
+			},
+		},
+		Networks: map[string]Network{
+			"default": {
+				Driver: "bridge",
+			},
+		},
+		Volumes: map[string]Volume{
+			"./" + project + "/data": {
+				Driver: "local",
+			},
+		},
+	}
+
+	d, err := yaml.Marshal(&composeFile)
+	if err != nil {
+		logger.Error("Error marshalling compose file: ", err)
+		return
+	}
+
+	logger.Debug("Compose file: ", string(d))
 
 }
--- readme.md
+++ readme.md
@@ -27,3 +27,68 @@ Deploys a specific tagged image on the machine
 1. -c, --config: Path to the configuration file
 2. -i, --image: Name of the image to deploy
 
+# Must have
+.env file with necessary environment variables
+deployer.yaml file with necessary configurations
+
+
+```yaml: deployer.yaml # Path: deployer.yaml
+name: Project Name
+projects:
+    - name: Project 1
+      env_path: .env
+      configs:
+        - name: Config 1
+          content: |
+            key: value
+    - name: Project 2
+      env_path: .env
+
+
+```
+
+# docker swarm setup
+```bash
+docker swarm init
+docker stack deploy -c docker-compose.yml <stack_name>
+```
+
+# docker-compose.yml
+```yaml: docker-compose.yml
+version: '3.7'
+
+services:
+  <service_name>:
+    image: <image_name>
+    ports:
+      - "8080:8080"
+    environment:
+      - key=value
+    networks:
+      - <network_name>
+    deploy:
+      replicas: 1
+      restart_policy:
+        condition: on-failure
+        delay: 5s
+        max_attempts: 3
+        window: 120s
+      update_config:
+        parallelism: 2
+        delay: 10s
+      placement:
+        constraints:
+          - node.role == worker
+      resources:
+        limits:
+          cpus: '0.50'
+          memory: 50M
+        reservations:
+          cpus: '0.25'
+          memory: 20M`
+
+networks:
+    <network_name>:
+        driver: overlay
+    
+```
